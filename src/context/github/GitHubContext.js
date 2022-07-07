e3127b5bbbb550c8571191fdb5944f51d1ce1fb8
laexy <alex31wk@gmail.com>
2022-07-07 12:41:10 +1000

    updated to useReducer instead of useState

--- src/context/github/GitHubContext.js
+++ src/context/github/GitHubContext.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+import gitHubReducer from "./GitHubReducer";
 
 const GitHubContext = createContext();
 
@@ -6,8 +7,12 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GitHubProvider = ({ children }) => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const initialState = {
+    users: [],
+    loading: true,
+  };
+
+  const [state, dispatch] = useReducer(gitHubReducer, initialState);
 
   const fetchUsers = async () => {
     const response = await fetch(`${GITHUB_URL}/users`, {
@@ -18,12 +23,13 @@ export const GitHubProvider = ({ children }) => {
 
     const data = await response.json();
 
-    setUsers(data);
-    setLoading(false);
+    dispatch({ type: "GET_USERS", payload: data });
   };
 
   return (
-    <GitHubContext.Provider value={{ users, loading, fetchUsers }}>
+    <GitHubContext.Provider
+      value={{ users: state.users, loading: state.loading, fetchUsers }}
+    >
       {children}
     </GitHubContext.Provider>
   );
