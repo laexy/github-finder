6307a1cb692c9d2fb5392603725da0dd2fb95357
laexy <alex31wk@gmail.com>
2022-07-11 15:40:07 +1000

    Grabbing more data and adding more styling to User.jsx

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,4 +1,7 @@
+import { FaCodepen, FaStore, FaUserFriends, FaUsers } from "react-icons/fa";
 import { useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import Spinner from "../components/layout/Spinner";
 import GitHubContext from "../context/github/GitHubContext";
 import { useParams } from "react-router-dom";
 
@@ -12,7 +15,67 @@ function User({}) {
     //getUserRepos(params.login);
   }, []);
 
-  return <div>{user.login}</div>;
+  //destructuring data from user object
+  const {
+    name,
+    type,
+    avatar_url,
+    location,
+    bio,
+    twitter_username,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user;
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <>
+      <div className="w-full mx-auto lg:w-10/12">
+        <div className="mb-4">
+          <Link to="/" className="btn btn-ghost">
+            Back to Search
+          </Link>
+        </div>
+
+        <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8">
+          <div className="custom-card-image mb-6 md:mb-0">
+            <div className="rounded-lg shadow-xl card image-full">
+              <figure>
+                <img src={avatar_url} alt="avatar" />
+              </figure>
+              <div className="card-body justify-end">
+                <h2 className="card-title mb-0">{name}</h2>
+                <p>{login}</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-2">
+            <div className="mb-6">
+              <h1 className="text-3xl card-title">
+                {name}]
+                <div className="ml-2 mr-1 badge badge-success">{type}</div>
+                {hireable && (
+                  <div className="mx-1 badge badge-info">Hireable</div>
+                )}
+              </h1>
+              <p>{bio}</p>
+              <div className="mt-4 card-actions">
+                <a href={html_url} target="_blank" rel="noreferrer" className="btn btn-outline">Visit GitHub Profile</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default User;
